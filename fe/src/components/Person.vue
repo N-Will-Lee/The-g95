5365960dfc655829ba0d481301eb40960bbf7c70
Jared Candelaria <jaredcandelaria@Jareds-MacBook-Pro.local>
2018-11-16 09:53:30 -0700

    vue up and running

---
+++ fe/src/components/Person.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="personProfile">
+        <h1>The G95-WebPage</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Person"
+}
+</script>
+
+<style>
+
+</style>
